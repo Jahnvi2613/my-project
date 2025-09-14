c6fa64807d4c4172c6c5600a80e79ab2e10d5f92
Jahnvi <23bai70330@cuchd.in>
2025-09-14 12:57:00 +0530

    commit

---
+++ React-projects/Experiment-8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Library from "./component/Library";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Library />
+    </div>
+  );
+}
+
+export default App;
